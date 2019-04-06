df319a73e626a2580dc5accd3c9b8b8e7f703282
2019 <2019@HOME-PC>
2019-04-06 09:49:56 -0300

    p

---
+++ src/colections/Pessoa.java
@@ -0,0 +1,16 @@
+package colections;
+
+public class Pessoa {
+	public static String exibirMaioridade(int idade) {
+		if(idade < 0) {
+			throw new IllegalArgumentException("A idade não pode ser negativa");
+		}
+		if(idade < 18) {
+			return "Menor de Idade";
+		}
+		else {
+			return "Maior de Idade";
+		}
+	}
+
+}
