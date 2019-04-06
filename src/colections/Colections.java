95be9ef8f0916915b8dcd85a8b04b597535b9589
2019 <2019@HOME-PC>
2019-04-06 08:47:17 -0300

    Update

--- src/colections/Colections.java
+++ src/colections/Colections.java
@@ -1,18 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package colections;
 
 import javax.swing.JOptionPane;
 
 public class Colections {
     public static void main(String[] args) {
+    	try {
     	String idade = JOptionPane.showInputDialog("Qual a sua idade? ");
     	int valorIdade = Integer.parseInt(idade);
     	System.out.print("Sua idade é " + valorIdade);
-    
-    }
-    
+    	} 
+    	
+    	catch (NumberFormatException e) {
+    		JOptionPane.showMessageDialog(null,e.getMessage());
+    		//System.out.println(e.getMessage());
+    	} 
+    }  
 }
